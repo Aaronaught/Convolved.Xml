7c55e0a303bfb1f04bdd142eec69965a7e64c09d
Aaron <convolved@outlook.com>
2012-08-11 17:16:19 -0400

    Initial commit

---
+++ Convolved.Xml.Serialization/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Convolved.Xml.Serialization")]
+[assembly: AssemblyDescription("XML Serialization Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Convolved")]
+[assembly: AssemblyProduct("Convolved.Xml")]
+[assembly: AssemblyCopyright("© 2012 Convolved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c2d868b-da95-4c73-9e17-c9ff31895d8c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Convolved.Xml.Serialization/TypeDiscoveryExtension.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Reflection;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using System.Xml.Serialization.Advanced;
+
+namespace Convolved.Xml.Serialization
+{
+    public class TypeDiscoveryExtension : SchemaImporterExtension
+    {
+        private Dictionary<string, Type> discoveredTypes;
+
+        public override string ImportSchemaType(string name, string ns, XmlSchemaObject context,
+            XmlSchemas schemas, XmlSchemaImporter importer, CodeCompileUnit compileUnit,
+            CodeNamespace mainNamespace, CodeGenerationOptions options,
+            CodeDomProvider codeProvider)
+        {
+            if (discoveredTypes == null)
+                DiscoverTypes();
+            string key = ns + name;
+            Type type;
+            if (!discoveredTypes.TryGetValue(key, out type))
+                return base.ImportSchemaType(name, ns, context, schemas, importer, compileUnit,
+                    mainNamespace, options, codeProvider);
+            compileUnit.ReferencedAssemblies.Add(type.Assembly.FullName);
+            XmlSchemaElement schemaElement = context as XmlSchemaElement;
+            if ((schemaElement != null) && schemaElement.IsNillable &&
+                (schemaElement.ElementSchemaType is XmlSchemaSimpleType))
+                return String.Format("System.Nullable<{0}>", type.FullName);
+            return type.FullName;
+        }
+
+        protected virtual void DiscoverTypes()
+        {
+            discoveredTypes = new Dictionary<string, Type>();
+            string discoveryDirectoryName =
+                Environment.GetEnvironmentVariable("ConvolvedXmlDiscoveryPath");
+            if (string.IsNullOrEmpty(discoveryDirectoryName))
+                discoveryDirectoryName = Environment.CurrentDirectory;
+            if (!Directory.Exists(discoveryDirectoryName))
+                return;
+            string[] fileNames = Directory.GetFiles(discoveryDirectoryName, "*.dll",
+                SearchOption.AllDirectories);
+            foreach (string fileName in fileNames)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(fileName);
+                    foreach (Type type in assembly.GetTypes())
+                    {
+                        XmlTypeAttribute xta = (XmlTypeAttribute)Attribute.GetCustomAttribute(type,
+                            typeof(XmlTypeAttribute), false);
+                        string ns = (xta != null) ? xta.Namespace : null;
+                        if (string.IsNullOrEmpty(ns))
+                            ns = "http://tempuri.org/";
+                        string name = (xta != null) ? xta.TypeName : null;
+                        if (string.IsNullOrEmpty(name))
+                            name = type.Name;
+                        string key = ns + name;
+                        discoveredTypes[key] = type;
+                    }
+                }
+                catch (BadImageFormatException)
+                {
+                    // Not a readable .NET assembly; ignore it.
+                }
+            }
+        }
+    }
+}
